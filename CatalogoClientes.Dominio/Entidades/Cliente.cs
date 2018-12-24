584955e9cdc814884b08f62b1ea8096935654ef0
Pablo Ferreira <Pablo_Ferreira14@hotmail.com>
2018-12-24 11:15:39 -0200

    .

--- CatalogoClientes.Dominio/Entidades/Cliente.cs
+++ CatalogoClientes.Dominio/Entidades/Cliente.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CatalogoClientes.Dominio.Entidades
 {
@@ -11,10 +7,19 @@ namespace CatalogoClientes.Dominio.Entidades
     public class Cliente
     {
         public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do cliente")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o e-mail do cliente")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe o endereço do cliente")]
         public string Endereco { get; set; }
+
         public byte[] Imagem { get; set; }
+
         public string Tipo { get; set; }
 
     }
---
+++ CatalogoClientes.Web/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using CatalogoClientes.Dominio.Entidades;
+using CatalogoClientes.Dominio.Repositorio;
+using System.Web.Mvc;
+
+namespace CatalogoClientes.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private IRepositorio<Cliente> _repositorioCliente;
+
+        public ClienteController()
+        {
+            _repositorioCliente = new ClienteRepositorio(new ClienteContexto());
+        }
+
+
+        public ActionResult Catalogo()
+        {
+            return View(_repositorioCliente.GetTodos());
+        }
+
+
+    }
+}
